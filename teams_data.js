112195b0d40b2236a2dfacbfa8f21ac9842fc479
harrb7 <ahmed7mody16@gmail.com>
2025-12-14 00:09:05 +0200

    Add files via upload

---
+++ teams_data.js
@@ -0,0 +1,24 @@
+// teams_data.js
+const TEAM_DATABASE = [
+    // --- CLUBS ---
+    { name: "Liverpool", logo: "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg" },
+    { name: "West Ham", logo: "https://upload.wikimedia.org/wikipedia/en/c/c2/West_Ham_United_FC_logo.svg" },
+    { name: "Arsenal", logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg" },
+    { name: "Man City", logo: "https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg" },
+    { name: "Real Madrid", logo: "https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg" },
+    { name: "Barcelona", logo: "https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg" },
+    { name: "Atletico Madrid", logo: "https://upload.wikimedia.org/wikipedia/en/f/f4/Atletico_Madrid_2017_logo.svg" },
+    { name: "Bayern Munich", logo: "https://upload.wikimedia.org/wikipedia/commons/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg" },
+    { name: "PSG", logo: "https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg" },
+    { name: "Fulham", logo: "https://upload.wikimedia.org/wikipedia/en/e/eb/Fulham_FC_%28shield%29.svg" },
+    
+    // --- COUNTRIES (Flags) ---
+    { name: "Egypt", logo: "https://flagcdn.com/w320/eg.png" },
+    { name: "Brazil", logo: "https://flagcdn.com/w320/br.png" },
+    { name: "Argentina", logo: "https://flagcdn.com/w320/ar.png" },
+    { name: "France", logo: "https://flagcdn.com/w320/fr.png" },
+    { name: "England", logo: "https://flagcdn.com/w320/gb-eng.png" },
+    { name: "Spain", logo: "https://flagcdn.com/w320/es.png" },
+    { name: "Germany", logo: "https://flagcdn.com/w320/de.png" },
+    { name: "Portugal", logo: "https://flagcdn.com/w320/pt.png" }
+];
